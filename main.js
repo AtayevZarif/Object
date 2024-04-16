4c3091a02b8522e81147cbd05e51597dcf7b7e0a
AtayevZarif <atayev1234567890@gmail.com>
2024-04-16 21:59:23 +0500

    Succses

---
+++ main.js
@@ -0,0 +1,11 @@
+let name = prompt("Ismingizni kiriting")
+let id = prompt("Modme idni kiriting")
+let passoword = prompt("Parolni kiriting")
+
+let data = {
+    name: name,
+    id: id, 
+    passoword: passoword,
+}
+
+console.log(data);
